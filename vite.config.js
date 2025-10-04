8220a6c56106d8fec6a8a8eb050f6994384d1cb8
Acla <daaruuuhh@gmail.com>
2025-10-05 00:20:53 +0800

    sueprmad

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // ✅ ensure production build goes here
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5174',
+        changeOrigin: true,
+      },
+    },
   },
 })
