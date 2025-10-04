60a643afb0b0fd7cc9f5e925c82a59c2adfa1f5b
Acla <daaruuuhh@gmail.com>
2025-10-05 00:38:42 +0800

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -11,5 +12,14 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 })
