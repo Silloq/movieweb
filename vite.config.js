29b6f7b9e04c9f2876a83280f1cb32a20f5adcf0
Acla <daaruuuhh@gmail.com>
2025-12-28 18:13:25 +0800

    SUPERMEGAMAD

--- vite.config.js
+++ vite.config.js
@@ -18,3 +18,18 @@ export default defineConfig({
     }),
   },
 })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5173,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5174',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// })
